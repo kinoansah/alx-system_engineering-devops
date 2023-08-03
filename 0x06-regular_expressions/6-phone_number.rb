499d149834b3bba07974ddd7600bdebb9bad838f
kinoansah <kinoansah@gmail.com>
2023-08-03 09:44:31 -0700

    Call me maybe

---
+++ 0x06-regular_expressions/6-phone_number.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+# Check if an argument is provided
+if ARGV.empty?
+  puts "Usage: #{$PROGRAM_NAME} <input_string>"
+  exit(1)
+end
+
+# Extract the input string from the command-line argument
+input_string = ARGV[0]
+
+# Define the regular expression pattern for a 10-digit phone number
+pattern = /^\d{10}$/
+
+# Match the pattern in the input string
+match_result = input_string.match(pattern)
+
+# Output the matched result or an empty string if no match found
+puts match_result ? match_result[0] : ""
+
