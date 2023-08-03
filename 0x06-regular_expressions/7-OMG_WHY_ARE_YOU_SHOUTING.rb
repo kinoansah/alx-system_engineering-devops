0acb8ab326f590ae19c8170c39c223e0c476d576
kinoansah <kinoansah@gmail.com>
2023-08-03 09:49:14 -0700

    OMG WHY ARE YOU SHOUTING?

---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+# Check if an argument is provided
+if ARGV.empty?
+  puts "Usage: #{$PROGRAM_NAME} <input_string>"
+  exit(1)
+end
+
+# Extract the input string from the command-line argument
+input_string = ARGV[0]
+
+# Define the regular expression pattern for matching capital letters
+pattern = /[A-Z]/
+
+# Match the pattern in the input string and join the results
+match_result = input_string.scan(pattern).join
+
+# Output the matched result or an empty string if no match found
+puts match_result
+
