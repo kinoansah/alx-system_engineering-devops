800fc3a71e7639f67c9809304437fea4701efa00
kinoansah <kinoansah@gmail.com>
2023-08-03 09:53:45 -0700

    Simply matching School

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,19 +1,3 @@
 #!/usr/bin/env ruby
-
-# Check if an argument is provided
-if ARGV.empty?
-  puts "Usage: #{$PROGRAM_NAME} <input_string>"
-  exit(1)
-end
-
-# Extract the input string from the command-line argument
-input_string = ARGV[0]
-
-# Define the regular expression pattern
-pattern = /School/
-
-# Match the pattern in the input string
-match_result = input_string.match(pattern)
-
-# Output the matched result or an empty string if no match found
-puts match_result ? match_result[0] : ""
+# A regular expression that is simply matching School
+puts ARGV[0].scan(/School/).join
